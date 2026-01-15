a6d2cf91a1879a48fe022d86ea45cba667cfdf3f
santosh majakoti <santoshjr08@gmail.com>
2026-01-15 13:00:55 +0545

    change type common js to module

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,16 @@
-const  express =require("express");
+import express from "express";
+import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config(); 
+const app = express();
 
-const app =express();
-const PORT =3000;
+// Middleware
+app.use(cors()); 
+app.use(express.json()); 
 
-app.listen(PORT,(req,res)=>{
-    console.log(`server is runnig at port:${PORT}`);
-})
+app.get("/", (req, res) => {
+  res.send("hello from server.");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
